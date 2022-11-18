07e7b75627679221dbf01b76648cda38284c6339
Mathcodefx <matheustempo21@gmail.com>
2022-11-18 15:01:09 -0300

    .

---
+++ DadosFuncionario/DadosFuncionario/InfoFuncionarios.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DadosFuncionario
+{
+    internal class InfoFuncionarios
+    {
+
+        public String Name;
+        public double SalarioBruto;
+        public double Imposto;
+       
+        
+
+        public double Salario()
+        {
+            return SalarioBruto - Imposto;
+        }
+
+        public void AumentarSalario(double porcent)
+        {
+            SalarioBruto = SalarioBruto + (SalarioBruto * porcent / 100.0);
+        }
+
+        public override string ToString()
+        {
+            return Name
+            + ", R$ "
+            + Salario().ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+
+
+
+
+
+    }
+}
---
+++ DadosFuncionario/DadosFuncionario/Program.cs
@@ -0,0 +1,40 @@
+﻿using DadosFuncionario;
+using System;
+using System.Globalization;
+
+
+namespace Program
+{
+    class DadosFuncionario
+    {
+        static void Main(String[] Args)
+        {
+            InfoFuncionarios f1 = new InfoFuncionarios();
+
+            Console.Write("Nome: ");
+            f1.Name = Console.ReadLine();
+            Console.Write("Salário Bruto: ");
+            f1.SalarioBruto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Imposto: ");
+            f1.Imposto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine();
+            Console.Write("Funcionario: " +f1);
+            Console.WriteLine();
+            Console.Write("Digite a porcentagem para aumentar o salário: ");
+            double porcent = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            f1.AumentarSalario(porcent);
+            Console.WriteLine();
+
+            Console.WriteLine("Dados Atualizados: " + f1);
+
+
+
+        }
+
+
+
+    }
+
+
+
+}
