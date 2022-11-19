07b4b117880696e6d7fa811df216f56e168db3d1
Mathcodefx <matheustempo21@gmail.com>
2022-11-18 21:29:16 -0300

    .

---
+++ NotasAluno/NotasAluno/Program.cs
@@ -0,0 +1,45 @@
+﻿using NotasAluno;
+using System;
+using System.Globalization;
+
+namespace Curso
+{
+    class AlunosNotas
+    {
+        static void Main(String[] Args)
+        {
+            Alunos aluno1 = new Alunos();
+            Alunos aluno2 = new Alunos();
+
+            Console.Write("Nome do Aluno: ");
+            aluno1.Nome = Console.ReadLine();
+            Console.WriteLine("Digite as três notas do aluno: ");
+            aluno1.nota1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            aluno1.nota2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            aluno1.nota3 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("NOTA FINAL = " + aluno1.NotaFinal().ToString("F2", CultureInfo.InvariantCulture));
+
+            if (aluno1.Aprovado())
+            {
+                Console.WriteLine("APROVADO");
+            }
+            else
+            {
+                Console.WriteLine("REPROVADO");
+                Console.WriteLine("FALTARAM "
+                + aluno1.Restante().ToString("F2", CultureInfo.InvariantCulture)
+                + " PONTOS");
+            }
+
+
+
+        }
+
+
+
+    }
+
+
+
+}
---
+++ NumeroRandom/NumeroRandom/Program.cs
@@ -0,0 +1,33 @@
+﻿/* criando sistema de numeros rands sem repetir os numeros */
+
+using System;
+
+namespace Testando
+{
+    class Program
+    {
+        static void Main(String[] Args)
+        {
+            Random NumRandom = new Random();
+
+
+            Console.Write("Digite a quantidade de numeros a serem sorteados: ");
+            int n = int.Parse(Console.ReadLine());
+            Console.Write("Digite o minimo de numeros a serem sorteados: ");
+            int min = int.Parse(Console.ReadLine());
+            Console.Write("Digite o maximo de numeros a serem sorteados: ");
+            int max = int.Parse(Console.ReadLine());
+
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine(" " + NumRandom.Next(min, max));
+            }
+        }
+
+
+
+    }
+
+
+}
